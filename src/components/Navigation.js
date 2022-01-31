3e7d90310b5cabc98b23a4da609a70c6ef2f313a
unknown <mccullochryan636@gmail.com>
2022-01-30 18:29:32 -0700

    creating components

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,17 +2,18 @@ import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 
 const Navigation = () => {
-  return (<Navbar bg="primary" variant="dark">
-    <Container>
-      <Navbar.Brand href="#home">Ryan McCulloch</Navbar.Brand>
-      <Nav className="me-auto">
-        <Nav.Link href="#aboutme">About Me</Nav.Link>
-        <Nav.Link href="#contact">Contact</Nav.Link>
-        <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-        <Nav.Link href="#resume">Resume</Nav.Link>
-      </Nav>
-    </Container>
-  </Navbar>)
+  return (
+    <Navbar className="color-nav" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Ryan McCulloch</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#aboutme">About Me</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
+          <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+          <Nav.Link href="#resume">Resume</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>)
 }
 
 export default Navigation
--- src/pages/Aboutme.js
+++ src/pages/Aboutme.js
@@ -5,13 +5,13 @@ const Aboutme = () => {
     return (
         <Figure>
             <Figure.Image
-                width={291}
-                height={320}
+                width={591}
+                height={620}
                 alt="171x180"
                 src="/images/Telluride.jpg"
             />
             <Figure.Caption>
-                Hi, welcome to my portfolio. My name is Ryan and I've been lerning how to code for almost six months.
+                Hi, welcome to my first React app. My name is Ryan and I've been lerning how to code for almost six months.
                 I've wanted to change carreers for a while and decided to give coding a shot. Thanks for visiting my page and stay tuned for more updates.
             </Figure.Caption>
         </Figure>
