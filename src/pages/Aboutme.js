1a6a293b08134d8ccb3fbb5b65e1ad5c46640aa4
unknown <mccullochryan636@gmail.com>
2022-02-02 17:00:05 -0700

    about me

--- src/pages/Aboutme.js
+++ src/pages/Aboutme.js
@@ -12,7 +12,10 @@ const Aboutme = () => {
             />
             <Figure.Caption>
                 Hi, welcome to my first React app. My name is Ryan and I've been lerning how to code for almost six months.
-                I've wanted to change carreers for a while and decided to give coding a shot. Thanks for visiting my page and stay tuned for more updates.
+                I've wanted to change carreers for a while and decided to give coding a shot. During my time in the DU coding bootcamp
+                I've learned about many different technologies including HTML5, CSS3, JavaScript, jQuery, Node.js, PHP, Laravel, Express.js, React.
+                Learning how to code has been a challenging yet rewarding journey and I'm exited to see where I go next. 
+                Thank you for visiting my page and stay tuned for more updates. 
             </Figure.Caption>
         </Figure>
     )
