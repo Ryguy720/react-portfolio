937b35edd02c3503127adb4b1f715d7d4bf5fcb3
unknown <mccullochryan636@gmail.com>
2022-02-04 17:51:32 -0700

    fixing stuff

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,28 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import Aboutme from '../pages/Aboutme';
+import Portfolio from '../pages/Portfolio';
+
+
 
 const Navigation = () => {
   return (
+    <div>
     <Navbar className="color-nav text-center" variant="dark">
       <Container className="text center">
         <Navbar.Brand href="#home">Ryan McCulloch</Navbar.Brand>
         <Nav className="ml-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#aboutme">About Me</Nav.Link>
           <Nav.Link href="#contact">Contact</Nav.Link>
           <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-          <Nav.Link href="#resume">Resume</Nav.Link>
         </Nav>
       </Container>
-    </Navbar>)
+
+    </Navbar>
+    </div>
+    )
 }
+
 
 export default Navigation
